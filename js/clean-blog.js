752671a318288d1c5504c519e0c1b6721190f8aa
David Camilo Sanchez Mora <david.sanchez96@hotmail.com>
2016-12-02 10:05:43 -0500

    update

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -9,35 +9,7 @@ $(function() {
     $("[data-toggle='tooltip']").tooltip();
 });
 
-// Contact Form Scripts
-
-
-$(window).load(function() {
-    var $container = $('.animate-grid .gallary-thumbs');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
-    $('.animate-grid .categories a').click(function() {
-        $('.animate-grid .categories .active').removeClass('active');
-        $(this).addClass('active');
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        return false;
-    });
-});
-
+// Contact Form Script
 
 $(function() {
 
@@ -1083,4 +1055,33 @@ jQuery(document).ready(function($) {
                 this.previousTop = currentTop;
             });
     }
+});
+
+
+
+
+$(window).load(function() {
+    var $container = $('.animate-grid .gallary-thumbs');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+    $('.animate-grid .categories a').click(function() {
+        $('.animate-grid .categories .active').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
+    });
 });
